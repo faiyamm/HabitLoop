5bff3eefa5929c7aaed8082be71ec1f6c673d61c
faiyamm <sofia.almaraz@uabc.edu.mx>
2025-11-22 00:12:48 -0800

    Class 2 - Assignment 2: Extend the reservation form + variables

--- HabitLoop/ReservationForm.swift
+++ HabitLoop/ReservationForm.swift
@@ -8,8 +8,72 @@
 import SwiftUI
 
 struct ReservationForm: View {
+    // constants
+    let restaurantName = "Gourmet Haven"
+    let maxGuest = 10
+    let maxChildren = 4
+    
+    // state variable - if the value changes, the view updates
+    @State private var userName = ""
+    @State private var guestCount = 1
+    @State private var phoneNumber = ""
+    @State private var previewText = ""
+    @State private var numberChildren = 0
+    @State private var occasion = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form{
+            // header
+            Section{
+                Text(restaurantName)
+                    .font(.title3)
+                    .bold()
+                
+                Text("Reservation form")
+                    .foregroundColor(.secondary)
+            }
+
+            // reservation details
+            Section(header: Text("Reservation Details")){
+                TextField("Name", text: $userName)
+                .textInputAutocapitalization(.words)
+                .autocorrectionDisabled(true)
+                
+                TextField("Occasion (Birthday, Anniversary, etc.): ", text: $occasion)
+
+                // use a constant
+                Stepper("Guest: \(guestCount)", value: $guestCount, in: 1...maxGuest)
+                
+                Stepper("Children: \(numberChildren)", value: $numberChildren, in: 0...maxChildren)
+            }
+
+            // phone number
+            Section(header: Text("Contact Information")) {
+                TextField("Phone", text: $phoneNumber)
+                    .keyboardType(.phonePad)
+            }
+
+            // preview confirmation
+            Section {
+                Button("Preview Reservation"){
+                    previewText =
+                    """
+                    Name: \(userName)
+                    Occasion: \(occasion)
+                    Guests: \(guestCount)
+                    Children: \(numberChildren)
+                    Phone: \(phoneNumber)
+                    """
+                }
+            }
+
+            // show preview text
+            Section(header: Text("Reservation Preview")){
+                Text(previewText)
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+            }
+        }
     }
 }
 
--- Practices/Variables.playground/Contents.swift
+++ Practices/Variables.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var restaurantName = "Cafe Delight"
+restaurantName = "Bistro Bliss"
+print(restaurantName)
+
+let city = "San Francisco"
+// city = "Los Angeles" // this line would cause a compile-time error
+print(city)
+
+// types of data
+// String -> text
+var specialDish = "Pasta"
+
+// Int -> whole number
+var availableTables = 5
+
+// Double -> decimal number
+var dishPrice = 12.99
+
+// Bool -> true or false
+var isOpen = true
+
+// interpolation
+print("Today's special dish: \(specialDish) - $\(dishPrice).")
+
+// mini challenge 1:
+var numberOfTables = 10
+let ownerName = "Mario"
+print("Little Lemon has \(numberOfTables) tables, owned by \(ownerName).")
+
+// mini challenge 2:  add a 15% tip to a restaurant bill
+let pastaPrice = 10.50
+let saladPrice = 6.25
+let subtotal = pastaPrice + saladPrice
+print("The total price is: $\(subtotal)")
+
+let tipPercentage = 0.15 * subtotal
+let total = subtotal + tipPercentage
+print("Total with 15% tip: $\(total)")
