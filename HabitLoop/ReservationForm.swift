75d0751b373563291db303b559f5e53f5846a663
faiyamm <sofia.almaraz@uabc.edu.mx>
2025-11-21 21:51:27 -0800

    habitloop about view

---
+++ HabitLoop/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  HabitLoop
+//
+//  Created by Fai on 21/11/25.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
