195ddce91bdff96eb60153fb020526e31f8108df
faiyamm <sofia.almaraz@uabc.edu.mx>
2025-11-21 21:19:51 -0800

    Initial Commit

---
+++ HabitLoop/HabitLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitLoopApp.swift
+//  HabitLoop
+//
+//  Created by Fai on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
